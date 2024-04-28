b2fb2ba2257e4702712fe982e22d0faeb35d3a3f
Changyao Chen <changyao.chen@instacart.com>
2024-04-28 17:24:40 -0400

    wip

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
+    path("hello/", include("hello.urls")),
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="home/main.html")),
+
 ]
