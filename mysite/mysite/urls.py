efd396d819dab69754197abb0a096ecd16f11ca6
Changyao Chen <changyao.chen@instacart.com>
2024-06-16 21:44:19 -0400

    wip

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path("hello/", include("hello.urls")),
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="home/main.html")),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('autos/', include('autos.urls')),
 
 ]
